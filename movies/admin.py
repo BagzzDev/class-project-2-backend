ba965618558e6270bbebea43b4c354b2640ab027
jjoseramoss <jr29beto12@gmail.com>
2026-04-26 21:54:57 -0500

    Completed SocialLink and Tweet models

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import SocialLink, Tweet
 
 # Register your models here.
+admin.site.register(SocialLink)
+admin.site.register(Tweet)
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import SocialLink, Tweet
 
 
 def index(request):
     """Render the homepage index."""
-    return render(request, 'index.html')
+    social_links = SocialLink.objects.all()
+    tweets = Tweet.objects.all()
+
+    context = {
+        'social_links': social_links,
+        'tweets': tweets,
+    }
+    return render(request, 'index.html', context)
