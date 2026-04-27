0e00d9a0b0764efa6314aadb0f4ad44fa1642b86
Rafael Anulao <rafael@alshival.ai>
2026-04-27 01:36:58 -0500

    added remaining models

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import SocialLink, Tweet
+from .models import SocialLink, Tweet, Slider, Advertisement, Trailer, TrailerItem, Celebrity, News, MovieTheater, MovieTV
 
 # Register your models here.
 admin.site.register(SocialLink)
 admin.site.register(Tweet)
+admin.site.register(Slider)
+admin.site.register(Advertisement)
+admin.site.register(Trailer)
+admin.site.register(TrailerItem)
+admin.site.register(Celebrity)
+admin.site.register(News)
+admin.site.register(MovieTheater)
+admin.site.register(MovieTV)
+
