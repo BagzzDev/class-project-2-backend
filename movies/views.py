66f131fb7c6c70fd6f79f1eb921d21525551fcd6
Ernesto Perez <ernesto.perez@startmail.com>
2026-04-29 15:33:17 -0500

    Add Trailer model to views
    Added the trailer model to the index view
    and only passes the first record.
    Edited the index.html to render the trailer obj.
    Then also edited the styling for importing from models.

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from .models import SocialLink, Tweet, Advertisement
+# from .models import SocialLink, Tweet, Advertisement
+from .models import (
+    SocialLink,
+    Tweet,
+    Advertisement,
+    Trailer
+)
 
 
 def index(request):
@@ -8,11 +14,13 @@ def index(request):
     tweets = Tweet.objects.all()
     sidebar_ad = Advertisement.objects.filter(section='sidebar').first()
     news_banner_ad = Advertisement.objects.filter(section='latestnews').first()
+    trailer = Trailer.objects.first()
 
     context = {
         'social_links': social_links,
         'tweets': tweets,
         'sidebar_ad': sidebar_ad,
-        'news_banner_ad': news_banner_ad
+        'news_banner_ad': news_banner_ad,
+        'trailer': trailer,
     }
     return render(request, 'index.html', context)
