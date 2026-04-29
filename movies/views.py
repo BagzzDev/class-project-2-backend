e38d1bbce8d64de5208821a9b44a4a3f5ff73851
Ernesto Perez <ernesto.perez@startmail.com>
2026-04-29 15:02:06 -0500

    Add Advertisement model to view
    Added the advertisment model to the index view.
    Edited the index.html to show the dynamic content.

--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from .models import SocialLink, Tweet
+from .models import SocialLink, Tweet, Advertisement
 
 
 def index(request):
     """Render the homepage index."""
     social_links = SocialLink.objects.all()
     tweets = Tweet.objects.all()
+    sidebar_ad = Advertisement.objects.filter(section='sidebar').first()
+    news_banner_ad = Advertisement.objects.filter(section='latestnews').first()
 
     context = {
         'social_links': social_links,
         'tweets': tweets,
+        'sidebar_ad': sidebar_ad,
+        'news_banner_ad': news_banner_ad
     }
     return render(request, 'index.html', context)
