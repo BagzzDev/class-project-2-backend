8a624c9eaf61a1847e41a0b4496b855614cf22d4
hectorcorp <hectorcorpus121505@gmail.com>
2026-04-29 21:48:48 -0500

    Added dymanic movie-tvs

--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from .models import SocialLink, Tweet
+from .models import SocialLink, Tweet, MovieTV
 
 
 def index(request):
     """Render the homepage index."""
     social_links = SocialLink.objects.all()
     tweets = Tweet.objects.all()
+    movie_tvs = MovieTV.objects.all()
 
     context = {
         'social_links': social_links,
         'tweets': tweets,
+        'movie_tvs': movie_tvs,
     }
     return render(request, 'index.html', context)
+
